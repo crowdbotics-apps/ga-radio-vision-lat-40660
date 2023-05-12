6fa46092379d5503b246d70ceb2e73540877b6c7
IURD IURD <radiogeorgia6081@gmail.com>
2023-05-12 15:38:58 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "ga-radio-vision-lat-40660",
   url: "https://ga-radio-vision-lat-40660.botics.co",
   api: "https://ga-radio-vision-lat-40660.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
